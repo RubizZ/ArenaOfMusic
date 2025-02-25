9496ffe869207b6fb729f826daa23c632d1104f5
Rubén <45830465+RubizZ@users.noreply.github.com>
2025-02-25 17:57:57 +0100

    Update karate-config.js
    Cambiado require de Node.js a os.name de Java

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -15,16 +15,14 @@ function fn() {
         baseUrl: 'http://localhost:8080'
     }
 
-    // Handle driver configuration based on OS platform (Linux or Windows)
-    var os = require('os');
-    var platform = os.platform(); // 'win32' for Windows, 'linux' for Ubuntu, etc.
+    var osName = Java.type('java.lang.System').getProperty('os.name').toLowerCase();
 
     /**
      * Drivers for tests - currently configured value is good for Linux
      */
     karate.configure('driver', {
         type: 'chrome',
-        executable: platform === 'win32' ? 
+        executable: osName.includes('win') ? 
             "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" : 
             '/usr/bin/google-chrome', // Path for Ubuntu/Linux (default location for Chrome)
         addOptions: ["--remote-allow-origins=*", "--incognito"],
