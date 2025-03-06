0e1d36edf2bce24550e5f06a137b2429b86479b2
CriDM <cristiandellamonica@gmail.com>
2025-03-06 15:30:15 +0100

    Tabla Usuario - fix import.sql
    Usuario -> IWUser

--- src/main/java/es/ucm/fdi/iw/model/User.java
+++ src/main/java/es/ucm/fdi/iw/model/User.java
@@ -1,15 +1,27 @@
 package es.ucm.fdi.iw.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import jakarta.persistence.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * An authorized user of the system.
@@ -18,26 +30,24 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @NamedQueries({
-        @NamedQuery(name="User.byUsername",
-                query="SELECT u FROM User u "
-                        + "WHERE u.username = :username AND u.enabled = TRUE"),
-        @NamedQuery(name="User.hasUsername",
-                query="SELECT COUNT(u) "
-                        + "FROM User u "
-                        + "WHERE u.username = :username")
+        @NamedQuery(name = "User.byUsername", query = "SELECT u FROM User u "
+                + "WHERE u.username = :username AND u.enabled = TRUE"),
+        @NamedQuery(name = "User.hasUsername", query = "SELECT COUNT(u) "
+                + "FROM User u "
+                + "WHERE u.username = :username")
 })
-@Table(name="IWUser")
+@Table(name = "IWUser")
 public class User implements Transferable<User.Transfer> {
 
     public enum Role {
-        USER,			// normal users 
-        ADMIN,          // admin users
+        USER, // normal users
+        ADMIN, // admin users
     }
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gen")
     @SequenceGenerator(name = "gen", sequenceName = "gen")
-	private long id;
+    private long id;
 
     @Column(nullable = false, unique = true)
     private String username;
@@ -47,18 +57,42 @@ public class User implements Transferable<User.Transfer> {
     private String firstName;
     private String lastName;
 
-    private boolean enabled;
+    @Column(nullable = false)
+    private boolean enabled = true;
+
     private String roles; // split by ',' to separate roles
 
-	@OneToMany
-	@JoinColumn(name = "sender_id")
-	private List<Message> sent = new ArrayList<>();
-	@OneToMany
-	@JoinColumn(name = "recipient_id")	
-	private List<Message> received = new ArrayList<>();		
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Integer EXP_totale = 0;
+
+    @Column(nullable = false)
+    private Integer EXP = 0;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationDateTime = new Date();
+
+    private Date lastLogin;
+
+    @Column(nullable = false)
+    private boolean banned = false;
+
+    @OneToMany
+    @JoinColumn(name = "sender_id")
+    private List<Message> sent = new ArrayList<>();
+
+    @OneToMany
+    @JoinColumn(name = "recipient_id")
+    private List<Message> received = new ArrayList<>();
 
     /**
      * Checks whether this user has a given role.
+     * 
      * @param role to check
      * @return true iff this user has that role.
      */
@@ -70,20 +104,38 @@ public class User implements Transferable<User.Transfer> {
     @Getter
     @AllArgsConstructor
     public static class Transfer {
-		private long id;
+        private long id;
         private String username;
-		private int totalReceived;
-		private int totalSent;
+        private String email;
+        private boolean enabled;
+        private boolean banned;
+        private int EXP_totale;
+        private int EXP;
+        private Date creationDateTime;
+        private Date lastLogin;
+        private int totalReceived;
+        private int totalSent;
     }
 
-	@Override
+    @Override
     public Transfer toTransfer() {
-		return new Transfer(id,	username, received.size(), sent.size());
-	}
-	
-	@Override
-	public String toString() {
-		return toTransfer().toString();
-	}
-}
+        return new Transfer(
+                id,
+                username,
+                email,
+                enabled,
+                banned,
+                EXP_totale,
+                EXP,
+                creationDateTime,
+                lastLogin,
+                received.size(),
+                sent.size());
+    }
 
+    @Override
+    public String toString() {
+        return toTransfer().toString();
+    }
+    
+}
