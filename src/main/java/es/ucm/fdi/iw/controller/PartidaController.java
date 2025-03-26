3d2f6398a36c4b0eee59e4aec554068cdf1a4d40
IvanAlcCam <alccamdev@gmail.com>
2025-03-26 19:47:18 +0100

    Comenzado controller config-partida

--- src/main/java/es/ucm/fdi/iw/controller/PartidaController.java
+++ src/main/java/es/ucm/fdi/iw/controller/PartidaController.java
@@ -1,5 +1,7 @@
 package es.ucm.fdi.iw.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,6 +26,7 @@ public class PartidaController {
 
     @GetMapping("/configuracion-partida")
     public String configPartida(Model model) {
+        model.addAttribute("playlists", partidaService.getActivePlaylists());
         return "configuracion-partida";
     }
 
