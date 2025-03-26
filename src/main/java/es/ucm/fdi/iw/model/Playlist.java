b98d0c47f0b923acb41420beab68b824566c55c2
IvanAlcCam <alccamdev@gmail.com>
2025-03-26 19:27:37 +0100

    Added get queries to Playlist entity

--- src/main/java/es/ucm/fdi/iw/model/Playlist.java
+++ src/main/java/es/ucm/fdi/iw/model/Playlist.java
@@ -10,6 +10,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -19,6 +21,10 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NamedQueries({
+        @NamedQuery(name = "Playlist.allPlaylist", query = "SELECT p FROM Playlist p"),
+        @NamedQuery(name = "Playlist.active", query = "SELECT p FROM Playlist p WHERE p.active = true"),
+})
 @NoArgsConstructor
 @Table(name = "playlist")
 public class Playlist implements Transferable<Playlist.Transfer> {
