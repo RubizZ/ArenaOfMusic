1462d193f7aa1b4bf266b69b387bc1de5acc2ef6
IvanAlcCam <ivanalca@ucm.es>
2025-03-31 07:11:16 +0200

    Added parse of gameConfig

--- src/main/java/es/ucm/fdi/iw/dto/GameConfigDTO.java
+++ src/main/java/es/ucm/fdi/iw/dto/GameConfigDTO.java
@@ -2,9 +2,11 @@ package es.ucm.fdi.iw.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class GameConfigDTO {
     long playlistId;
 
@@ -13,4 +15,20 @@ public class GameConfigDTO {
     int rounds;
 
     int fragmentDuration;
+
+    public void parseGameConfigDTO(String gameConfig) {
+        String[] parts = gameConfig.split(",");
+        this.playlistId = Long.parseLong(parts[0].split("=")[1]);
+        this.gameMode = parts[1].split("=")[1];
+        this.rounds = Integer.parseInt(parts[2].split(gameConfig)[1]);
+        this.fragmentDuration = Integer.parseInt(parts[3].split("=")[1]);
+    }
+
+    @Override
+    public String toString() {
+        return  "playlistId=" + playlistId +
+                ", gameMode='" + gameMode +
+                ", rounds=" + rounds +
+                ", fragmentDuration=" + fragmentDuration;
+    }
 }
