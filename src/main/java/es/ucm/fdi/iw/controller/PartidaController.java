233b643d5058ca16eafc515d979be35014cad684
IvanAlcCam <ivanalca@ucm.es>
2025-04-08 00:11:46 +0200

    Avanzado controller hasta get partida

--- src/main/java/es/ucm/fdi/iw/controller/PartidaController.java
+++ src/main/java/es/ucm/fdi/iw/controller/PartidaController.java
@@ -1,28 +1,32 @@
 package es.ucm.fdi.iw.controller;
 
-import java.util.List;
-import java.util.Map;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.util.UUID;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import es.ucm.fdi.iw.dto.game.GameConfigDTO;
+import es.ucm.fdi.iw.dto.game.GamePlayerDTO;
+import es.ucm.fdi.iw.model.Game;
+import es.ucm.fdi.iw.model.Playlist;
+import es.ucm.fdi.iw.model.User;
 import es.ucm.fdi.iw.service.PartidaService;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PartidaController {
 
     @Autowired
     private PartidaService partidaService;
-
-    private static final Logger log = LogManager.getLogger(PartidaController.class);
 
     @ModelAttribute
     public void populateModel(HttpSession session, Model model) {
@@ -31,51 +35,125 @@ public class PartidaController {
         }
     }
 
-    @GetMapping("/configuracion-partida")
+    @GetMapping("/partida/configuracion-partida")
     public String configPartida(Model model) {
-        // Playlists disponibles
-        model.addAttribute("playlists", partidaService.getPlaylists());
-
+        model.addAttribute("playlists", partidaService.getActivePlaylists());
         return "configuracion-partida";
     }
 
-    @GetMapping("/sala-espera")
-    public String salaespera(Model model, HttpSession session)
-    {
-        // Configuración de la sesión
-        model.addAttribute("selectedPlaylist", session.getAttribute("selectedPlaylist"));
-        model.addAttribute("rounds", session.getAttribute("rounds"));
-        model.addAttribute("time", session.getAttribute("time"));
-        model.addAttribute("mode", session.getAttribute("mode"));
-
-        // Obtener información adicional de la partida
-        Map<String, Object> playlist = partidaService.getPlaylistInfo();
-        List<Map<String, Object>> players = partidaService.getPlayers();
-        Map<String, Object> host = partidaService.getHost();
-        Map<String, Object> game = partidaService.getGameInfo();
-        boolean isHost = true;
-
-        model.addAttribute("playlist", playlist);
-        model.addAttribute("players", players);
-        model.addAttribute("host", host);
-        model.addAttribute("game", game);
-        model.addAttribute("isHost", isHost);
-
-        log.info(
-            "Sala de espera con código: {}, modo: {}",
-            game.get("code"),
-            session.getAttribute("mode") != null ? session.getAttribute("mode") : "options"
-        );
-
-        return "sala-espera";
+    @PostMapping("/partida/crear-partida")
+    public String crearPartida(Model model,
+            @RequestParam Long playlistId,
+            @RequestParam int rondas,
+            @RequestParam int tiempo,
+            @RequestParam String modoJuego,
+            RedirectAttributes redirectAttributes,
+            HttpSession session) {
+        User creator = (User) session.getAttribute("u");
+        GameConfigDTO gameConfig = new GameConfigDTO(playlistId, modoJuego, rondas, tiempo);
+        try {
+            UUID gameId = partidaService.crearPartida(gameConfig, creator.getId());
+            return "redirect:/partida/sala-espera/" + gameId.toString();
+        } catch (IllegalArgumentException e) {
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+            return "redirect:/error";
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "Error al crear la partida.");
+            return "redirect:/error";
+        }
     }
 
-    @GetMapping("/partida")
-    public String partida(Model model) {
-        return "partida";
+    @GetMapping("/partida/sala-espera/{gameId}")
+    public String salaEspera(@PathVariable UUID gameId, Model model, HttpSession session) {
+        try {
+            Game game = partidaService.getGameById(gameId);
+
+            if (game == null || !game.getActive()) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "La partida no existe.");
+            }
+
+            if (game.getGameState().equals(Game.GameState.FINISHED)) {
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN, "La partida ya ha finalizado.");
+            }
+
+            if (!game.getGameState().equals(Game.GameState.WAITING)) {
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN, "La partida ya ha comenzado.");
+            }
+
+            // Obtener Configuracion de la Partida
+            String gameConfigString = game.getConfigJson();
+            GameConfigDTO gameConfig = new GameConfigDTO();
+            gameConfig.parseGameConfigDTO(gameConfigString);
+            // Obtener jugadores de la partida
+            Set<GamePlayerDTO> players = partidaService.getGamePlayers(gameId);
+            // Obtener información de la playlist
+            Playlist playlist = game.getPlaylist();
+
+            // Agregar datos al modelo
+            model.addAttribute("gameId", game.getId().toString());
+            model.addAttribute("players", players);
+            model.addAttribute("gameConfig", gameConfig);
+            model.addAttribute("playlist", playlist);
+            return "sala-espera";
+        } catch (ResponseStatusException e) {
+            model.addAttribute("msg", "Error al acceder a la sala de espera: " + e.getReason());
+            model.addAttribute("status", e.getStatusCode().value());
+            return "error";
+        } catch (Exception e) {
+            model.addAttribute("msg", "Error al acceder a la sala de espera: " + e.getMessage());
+            return "error";
+        }
     }
 
-    @GetMapping("/resultados")
+    @PostMapping("/partida/iniciar/{gameId}")
+    public String iniciarPartida(@PathVariable UUID gameId, RedirectAttributes redirectAttributes) {
+        Game game = partidaService.getGameById(gameId);
+        if (game == null || !game.getActive()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "La partida no existe.");
+        }
+
+        if (game.getGameState().equals(Game.GameState.FINISHED)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "La partida ya ha finalizado.");
+        }
+
+        if (!game.getGameState().equals(Game.GameState.WAITING)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "La partida ya ha comenzado.");
+        }
+
+        try {
+            partidaService.startGame(gameId);
+            return "redirect:/partida/partida/" + gameId.toString();
+        } catch (IllegalArgumentException e) {
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+            return "redirect:/error";
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "Error al iniciar la partida.");
+            return "redirect:/error";
+        }
+    }
+
+    @GetMapping("/partida/partida/{gameId}")
+    public String partida(@PathVariable UUID gameId, Model model) {
+        try {
+            Game game = partidaService.getGameById(gameId);
+            if (game == null || !game.getActive()) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "La partida no existe.");
+            }
+            if (game.getGameState().equals(Game.GameState.FINISHED)) {
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN, "La partida ya ha finalizado.");
+            }
+            if (game.getGameState().equals(Game.GameState.WAITING)) {
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN, "La partida no ha comenzado.");
+            }
+            return "partida";
+        } catch (ResponseStatusException e) {
+            model.addAttribute("msg", "Error al acceder a la sala de espera: " + e.getReason());
+            model.addAttribute("status", e.getStatusCode().value());
+            return "error";
+        }
+    }
+
+    @GetMapping("/partida/resultados")
     public String resultados(Model model) {
         model.addAttribute("position", "¡Has acabado en 1ª posición!");
         model.addAttribute("playlist", partidaService.getPlaylist());
@@ -85,19 +163,4 @@ public class PartidaController {
         return "resultados";
     }
 
-    @PostMapping("/guardar-configuracion")
-    public String guardarConfiguracion(
-        @RequestParam("selectedPlaylist") String selectedPlaylist,
-        @RequestParam("rounds") int rounds,
-        @RequestParam("time") int time,
-        @RequestParam("mode") String mode,
-        HttpSession session)
-    {
-        session.setAttribute("selectedPlaylist", selectedPlaylist);
-        session.setAttribute("rounds", rounds);
-        session.setAttribute("time", time);
-        session.setAttribute("mode", mode);
-
-        return "redirect:/sala-espera";
-    }
 }
