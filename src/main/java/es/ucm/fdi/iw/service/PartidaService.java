9dd585d7f9eea6f1bf59b5218ed255d41b908115
IvanAlcCam <ivanalca@ucm.es>
2025-04-08 00:12:14 +0200

    Avanzado partida serivice

--- src/main/java/es/ucm/fdi/iw/service/PartidaService.java
+++ src/main/java/es/ucm/fdi/iw/service/PartidaService.java
@@ -2,24 +2,33 @@ package es.ucm.fdi.iw.service;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+
+import es.ucm.fdi.iw.dto.game.GameConfigDTO;
+import es.ucm.fdi.iw.dto.game.GamePlayerDTO;
+import es.ucm.fdi.iw.model.Game;
+import es.ucm.fdi.iw.model.PlayerGame;
+import es.ucm.fdi.iw.model.PlayerGameId;
+import es.ucm.fdi.iw.model.Playlist;
+import es.ucm.fdi.iw.model.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PartidaService {
 
-    public List<Map<String, Object>> getPlaylists() {
-        return Arrays.asList(
-                Map.of("id", 1, "image", "img/default-profile.png", "title", "The Weeknd", "author", "ArenaOfMusic"),
-                Map.of("id", 2, "image", "img/default-profile.png", "title", "ColdPlay", "author", "ArenaOfMusic"),
-                Map.of("id", 3, "image", "img/default-profile.png", "title", "Clásicos 80's", "author", "ArenaOfMusic"),
-                Map.of("id", 4, "image", "img/default-profile.png", "title", "Post Malone", "author", "ArenaOfMusic"),
-                Map.of("id", 5, "image", "img/default-profile.png", "title", "Country", "author", "ArenaOfMusic"),
-                Map.of("id", 6, "image", "img/default-profile.png", "title", "Ed Sheeran", "author", "ArenaOfMusic")
-        );
-    }
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public Map<String, Object> getPlaylist() {
         Map<String, Object> playlist = new HashMap<>();
@@ -71,7 +80,8 @@ public class PartidaService {
                         "winnerName", "Eric",
                         "time", 3.9),
                 Map.of(
-                        "song", Map.of("name", "Memories", "artist", "David Guetta ft. Kid Cudi"),
+                        "song",
+                        Map.of("name", "Memories", "artist", "David Guetta ft. Kid Cudi"),
                         "winnerName", "Eric",
                         "time", 3.5),
                 Map.of(
@@ -79,39 +89,185 @@ public class PartidaService {
                         "winnerName", "Sam",
                         "time", 4.8),
                 Map.of(
-                        "song", Map.of("name", "Can't Hold Us", "artist", "Macklemore ft. Ryan Lewis"),
+                        "song",
+                        Map.of("name", "Can't Hold Us", "artist", "Macklemore ft. Ryan Lewis"),
                         "winnerName", "Ava",
                         "time", 2.7));
         return songResults;
 
     }
 
-    public Map<String, Object> getPlaylistInfo() {
-        return Map.of(
-            "title", "Top Hits 2010's",
-            "image", "img/logo.jpeg"
-        );
+    public List<Playlist> getActivePlaylists() {
+        TypedQuery<Playlist> playlists = entityManager.createNamedQuery("Playlist.active", Playlist.class);
+        return playlists.getResultList();
     }
 
-    public List<Map<String, Object>> getPlayers() {
-        return List.of(
-            Map.of("username", "Ava", "photoUrl", "img/default-profile.png", "level", 4, "winRate", 85),
-            Map.of("username", "Sam", "photoUrl", "img/default-profile.png", "level", 3, "winRate", 75),
-            Map.of("username", "Taylor", "photoUrl", "img/default-profile.png", "level", 8, "winRate", 71)
-        );
+    @Transactional
+    public UUID createPartida(GameConfigDTO gameConfig) {
+        try {
+            Playlist playlist = entityManager.find(Playlist.class, gameConfig.getPlaylistId());
+            if (playlist == null || !playlist.isActive()) {
+                throw new IllegalArgumentException(
+                        "La playlist seleccionada no existe o no se encuentra disponible.");
+            }
+
+            Game game = new Game();
+            game.setConfigJson(gameConfig.toString());
+            game.setRoundJson("[]");
+            game.setGameState(Game.GameState.WAITING);
+            game.setPlaylist(playlist);
+
+            entityManager.persist(game);
+
+            return game.getId();
+        } catch (Exception e) {
+            System.err.println("Error al crear la partida: " + e.getMessage());
+            throw new RuntimeException("No se pudo crear la partida, intenta nuevamente.");
+        }
     }
 
-    public Map<String, Object> getHost() {
-        return Map.of("username", "Eric", "photoUrl", "img/default-profile.png", "level", 4, "winRate", 38);
+    public Game getGameById(UUID gameId) {
+        Game game = entityManager.find(Game.class, gameId);
+        if (game == null || !game.getActive()) {
+            throw new IllegalArgumentException("La partida no existe.");
+        }
+        return game;
     }
 
-    public Map<String, Object> getGameInfo() {
-        return Map.of(
-            "code", "XT8V5L",
-            "status", "waiting",
-            "mode", "options",
-            "rounds", 20,
-            "songTime", 10
-        );
+    @Transactional
+    public PlayerGame addPlayerIntoGame(long userId, UUID gameId) {
+        try {
+            Game game = entityManager.find(Game.class, gameId);
+            if (game == null) {
+                throw new IllegalArgumentException("La partida con ID " + gameId + " no existe.");
+            }
+
+            User user = entityManager.find(User.class, userId);
+            if (user == null) {
+                throw new IllegalArgumentException("El usuario con ID " + userId + " no existe.");
+            }
+
+            PlayerGameId checkId = new PlayerGameId(gameId, userId);
+            if (entityManager.find(PlayerGame.class, checkId) != null) {
+                System.out.println("El usuario " + userId + " ya está en la partida " + gameId);
+                throw new IllegalStateException("El usuario ya está en esta partida.");
+            }
+
+            PlayerGame playerGame = new PlayerGame();
+
+            playerGame.setGame(game);
+            playerGame.setUser(user);
+
+            PlayerGameId playerGameId = new PlayerGameId(game.getId(), user.getId());
+            playerGame.setId(playerGameId);
+
+            playerGame.setScore(0);
+            playerGame.setPosition(0);
+
+            entityManager.persist(playerGame);
+
+            return playerGame;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Argumento invalido: " + e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            System.out.println(
+                    "Error inesperado al agregar jugador " + userId + " a partida " + gameId + "\n" + e.getMessage());
+            throw new RuntimeException("Error inesperado en el servidor al agregar el jugador al juego.", e);
+        }
+
     }
+
+    @Transactional
+    public void addPlayerGameToUser(long userId, PlayerGame pg) {
+        User user = entityManager.find(User.class, userId);
+        if (user == null) {
+            throw new IllegalArgumentException("El usuario con ID " + userId + " no existe.");
+        }
+        user.addPlayerGame(pg);
+    }
+
+    @Transactional
+    public void addPlayerGameToGame(UUID gameId, PlayerGame pg) {
+        Game game = entityManager.find(Game.class, gameId);
+        if (game == null) {
+            throw new IllegalArgumentException("La partida con ID " + gameId + " no existe.");
+        }
+        game.addPlayerGame(pg);
+    }
+    @Transactional
+    public UUID crearPartida(GameConfigDTO gameConfig, long playerId) {
+        try {
+            UUID gameId = createPartida(gameConfig);
+            PlayerGame pg = addPlayerIntoGame(playerId, gameId);
+            addPlayerGameToUser(playerId, pg);
+            addPlayerGameToGame(gameId, pg);
+            return gameId;
+        } catch (Exception e) {
+            System.err.println("Error al crear partida o vincular host con partida: " + e.getMessage());
+            throw new RuntimeException("No se pudo crear partida o vincular host a partida, intenta nuevamente.");
+        }
+    }
+
+    @Transactional
+    public void addPlayerToGame(UUID gameId, long userId) {
+        try {
+            Game game = entityManager.find(Game.class, gameId);
+            if (game == null) {
+                throw new IllegalArgumentException("La partida con ID " + gameId + " no existe.");
+            }
+
+            User user = entityManager.find(User.class, userId);
+            if (user == null) {
+                throw new IllegalArgumentException("El usuario con ID " + userId + " no existe.");
+            }
+
+            PlayerGame pg = addPlayerIntoGame(userId, gameId);
+            addPlayerGameToUser(userId, pg);
+            addPlayerGameToGame(gameId, pg);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Argumento invalido: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error inesperado al agregar jugador a la partida: " + e.getMessage());
+        }
+    }
+
+    public Set<GamePlayerDTO> getGamePlayers(UUID gameId) {
+        Game game = entityManager.find(Game.class, gameId);
+        if (game == null) {
+            throw new IllegalArgumentException("La partida no existe.");
+        }
+        List<PlayerGame> gamePlayers = game.getParticipants();
+        Set<GamePlayerDTO> players = new HashSet<>();
+
+        for (PlayerGame playerGame : gamePlayers) {
+            User player = entityManager.find(User.class, playerGame.getUser().getId());
+            GamePlayerDTO playerDTO = new GamePlayerDTO();
+            playerDTO.setId(player.getId());
+            playerDTO.setUsername(player.getUsername());
+            players.add(playerDTO);
+        }
+
+        return players;
+    }
+
+    @Transactional
+    public void startGame(UUID gameId) {
+        try {
+            Game game = entityManager.find(Game.class, gameId);
+            if (game == null) {
+                throw new IllegalArgumentException("La partida no existe.");
+            }
+            if (!game.getGameState().equals(Game.GameState.WAITING)) {
+                throw new IllegalStateException("La partida ya ha comenzado o ha finalizado.");
+            }
+            game.setGameState(Game.GameState.PLAYING);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Argumento invalido: " + e.getMessage());
+        } catch (IllegalStateException e) {
+            System.out.println("Invalid state: " + e.getMessage());
+        }
+
+    }
+
 }
