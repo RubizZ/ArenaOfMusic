d4e6b9a4e187d7c2633ad30f465d6fd9f7d73adb
IvanAlcCam <ivanalca@ucm.es>
2025-04-07 23:36:42 +0200

    Created DTOs for game

---
+++ src/main/java/es/ucm/fdi/iw/dto/game/GameConfigDTO.java
@@ -0,0 +1,34 @@
+package es.ucm.fdi.iw.dto.game;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GameConfigDTO {
+    long playlistId;
+
+    String gameMode;
+
+    int rounds;
+
+    int fragmentDuration;
+
+    public void parseGameConfigDTO(String gameConfig) {
+        String[] parts = gameConfig.split(",");
+        this.playlistId = Long.parseLong(parts[0].split("=")[1]);
+        this.gameMode = parts[1].split("=")[1];
+        this.rounds = Integer.parseInt(parts[2].split("=")[1]);
+        this.fragmentDuration = Integer.parseInt(parts[3].split("=")[1]);
+    }
+
+    @Override
+    public String toString() {
+        return  "playlistId=" + playlistId +
+                ", gameMode='" + gameMode +
+                ", rounds=" + rounds +
+                ", fragmentDuration=" + fragmentDuration;
+    }
+}
