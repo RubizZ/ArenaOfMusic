18f808dbab3dc06be9e72ec383615c87d62715b8
IvanAlcCam <ivanalca@ucm.es>
2025-04-07 23:36:21 +0200

    Added NamedQueries

--- src/main/java/es/ucm/fdi/iw/model/Playlist.java
+++ src/main/java/es/ucm/fdi/iw/model/Playlist.java
@@ -10,6 +10,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -20,6 +22,10 @@ import lombok.NoArgsConstructor;
 @Entity
 @Data
 @NoArgsConstructor
+@NamedQueries({
+    @NamedQuery(name = "Playlist.allPlaylist", query = "SELECT p FROM Playlist p"),
+    @NamedQuery(name = "Playlist.active", query = "SELECT p FROM Playlist p WHERE p.active = true"),
+})
 @Table(name = "playlist")
 public class Playlist implements Transferable<Playlist.Transfer> {
 
